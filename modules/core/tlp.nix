43ebb28ed2ae5183893d923c9c4a252119b01259
vvaaqqw <3254549695@qq.com>
2025-02-05 10:55:59 +0800

    .

--- modules/core/tlp.nix
+++ modules/core/tlp.nix
@@ -17,7 +17,7 @@
 
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      STOP_CHARGE_THRESH_BAT0 = 50; # 50 and above it stops charging
       # 开机默认关闭蓝牙
       DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
     };
