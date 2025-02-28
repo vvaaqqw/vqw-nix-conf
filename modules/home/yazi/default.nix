ef32cdf1826f3271261138d380506b77f5964bbb
vvaaqqw <3254549695@qq.com>
2025-02-28 22:38:24 +0800

    .

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -8,7 +8,7 @@
 		owner = "yazi-rs";
 		repo = "plugins";
 		rev = "5186af7984aa8cb0550358aefe751201d7a6b5a8";
-		hash = "";
+		hash = "sha256-Cw5iMljJJkxOzAGjWGIlCa7gnItvBln60laFMf6PSPM=";
 	};
 in
  {
