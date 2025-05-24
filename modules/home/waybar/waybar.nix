1a19876e27da0d7a1364d778214c79823777c766
vvaaqqw <3254549695@qq.com>
2025-05-24 15:53:19 +0800

    ,

--- modules/home/waybar/waybar.nix
+++ modules/home/waybar/waybar.nix
@@ -2,7 +2,7 @@
   programs.waybar = {
     enable = true;
   };
-  programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
-    mesonFlags = (oa.mesonFlags or []) ++ ["-Dexperimental=true"];
-  });
+  # programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
+  #   mesonFlags = (oa.mesonFlags or []) ++ ["-Dexperimental=true"];
+  # });
 }
