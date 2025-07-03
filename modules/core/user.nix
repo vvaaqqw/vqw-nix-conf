df75a3667ec2a5dd712005610299ca9bbced1686
vvaaqqw <3254549695@qq.com>
2025-07-03 23:19:32 +0800

    ,

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -8,7 +8,7 @@
   ...
 }: {
   imports = [inputs.home-manager.nixosModules.home-manager
-  inputs.nixvim.homeManagerModules.nixvim
+  inputs.nixvim.homeModules.nixvim
   ];
   home-manager = {
     useUserPackages = true;
