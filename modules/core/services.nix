e2c38af395226c1b65699510ccb94e0879100e39
vvaaqqw <3254549695@qq.com>
2025-08-31 10:38:11 +0800

    .

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -60,7 +60,7 @@
     };
     ############################
   };
-  services.logind.extraConfig = ''
+  services.logind.settings.Login = ''
     # don’t shutdown when power button is short-pressed
     HandlePowerKey=ignore
   '';
