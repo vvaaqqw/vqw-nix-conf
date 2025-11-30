b2fc7acd3ef04609e6fabffe5baca0cd4b6947f9
vvaaqqw <3254549695@qq.com>
2025-11-30 12:37:18 +0800

    .

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -10,7 +10,7 @@
   # programs.hyprland.portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   xdg.portal = {
     enable = true;
-    wlr.enable = true; #Whether to enable desktop portal for wlroots-based desktops.
+    # wlr.enable = true; #Whether to enable desktop portal for wlroots-based desktops.
     #xdgOpenUsePortal = true; # I need to provide a portal
     extraPortals = [
       # flatpak needs it
@@ -18,7 +18,7 @@
       pkgs.xdg-desktop-portal-gtk
       # niri needs it
       pkgs.xdg-desktop-portal-gnome
-      pkgs.kdePackages.xdg-desktop-portal-kde # 部分 KDE 应用需要
+      # pkgs.kdePackages.xdg-desktop-portal-kde # 部分 KDE 应用需要
     ];
   };
 
