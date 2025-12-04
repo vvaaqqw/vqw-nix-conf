21bdd7531ae2dfa3733e8ddf90916e7c2ee920a2
vvaaqqw <3254549695@qq.com>
2025-12-04 21:38:08 +0800

    .

--- modules/home/niri/variables.nix
+++ modules/home/niri/variables.nix
@@ -7,7 +7,8 @@
     _JAVA_AWT_WM_NONEREPARENTING = "1";
     # SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
     DISABLE_QT5_COMPAT = "0";
-    GDK_BACKEND = "wayland";
+    # Do not set the GDK_BACKEND environment variable globally as this will break the screencast portal.
+    # GDK_BACKEND = "wayland";
     ANKI_WAYLAND = "1";
     DIRENV_LOG_FORMAT = "";
     WLR_DRM_NO_ATOMIC = "1";
