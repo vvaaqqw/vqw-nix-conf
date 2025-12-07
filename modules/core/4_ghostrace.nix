8b99c2c697b8d19cb89d1cb21b67501b30954158
vvaaqqw <3254549695@qq.com>
2025-12-07 18:35:46 +0800

    .

--- modules/core/4_ghostrace.nix
+++ modules/core/4_ghostrace.nix
@@ -3,8 +3,13 @@
   pkgs,
   ...
 }: {
-  # firmware bug
-  boot.kernelParams = ["module_blacklist=ucsi_acpi"];
+  boot.kernelParams = [
+  # firmware bug: ucsi_acpi USBC000:00: possible UCSI driver bug 2
+  "module_blacklist=ucsi_acpi"
+  # Avoid touchpad click to tap (clickpad) bug. For more detail see:
+  # https://wiki.archlinux.org/title/Touchpad_Synaptics#Touchpad_does_not_work_after_resuming_from_hibernate/suspend
+  "psmouse.synaptics_intertouch=0"
+  ];
   # 只有thinkpad能用的命令
   services.thinkfan = {
     enable = true;
