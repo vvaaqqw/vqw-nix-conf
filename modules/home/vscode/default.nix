562565e203678551133083295e561f6f72b45c5f
vvaaqqw <3254549695@qq.com>
2026-03-15 10:56:14 +0800

    .

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -4,17 +4,19 @@
   ...
 }:
 {
-    imports =
-    [(import ./vscode-plugins.nix)]
-    ++ [(import ./vscode-settings.nix)];
+  # giving up manage vscode with nix
+    # imports =
+    # [(import ./vscode-plugins.nix)]
+    # ++ [(import ./vscode-settings.nix)];
 
   programs.vscode = {
     enable = true;
-    mutableExtensionsDir = true; #Whether extensions can be installed or updated manually or by Visual Studio Code. Mutually exclusive to programs.vscode.profiles.
     # package = pkgs.vscodium;
-    package = pkgs.vscode-fhs; #need fhs for codelldb
-    profiles.default.enableExtensionUpdateCheck = true;
-    profiles.default.enableUpdateCheck = false;
-    
+    # package = pkgs.vscode-fhs; #need fhs for codelldb
+
+    # Adding extension-specific dependencies, these will be added to the FHS environment: 
+    # needed for rust lang server and rust-analyzer extension
+    package = pkgs.vscode.fhsWithPackages (ps: with ps; [ rustup zlib openssl.dev pkg-config ]);
+    # mutableExtensionsDir = true; #Whether extensions can be installed or updated manually or by Visual Studio Code. Mutually exclusive to programs.vscode.profiles.
   };
 }
